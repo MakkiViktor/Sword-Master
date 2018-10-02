a3f5406b2019a8f9bcd876dc2656c4a28f619bc8
Udvari Tibor <udvaritibor95@gmail.com>
2018-10-02 11:34:54 +0200

    StanceDatabase and StanceData scriptable objects added.

---
+++ Assets/Scripts/StanceData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public class StanceData : ScriptableObject
+{
+	#if UNITY_EDITOR
+	[MenuItem("Tools/Create/Stance Data")]
+	public static void Create()
+	{
+		StanceData asset = CreateInstance<StanceData>();
+		AssetDatabase.CreateAsset(asset, "Assets/StanceData.asset");
+		AssetDatabase.SaveAssets();
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = asset;
+	}
+	#endif
+
+	public string stanceName;
+	public int randomNumberAboutThisStance;
+}
---
+++ Assets/Scripts/StanceDatabase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class StanceDatabase : ScriptableObject
+{
+	#if UNITY_EDITOR
+	[MenuItem("Tools/Create/Stance Database")]
+	public static void Create()
+	{
+		StanceDatabase asset = CreateInstance<StanceDatabase>();
+		AssetDatabase.CreateAsset(asset, "Assets/StaceDatabase.asset");
+		AssetDatabase.SaveAssets();
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = asset;
+	}
+	#endif
+
+	public List<StanceData> stanceList;
+}
