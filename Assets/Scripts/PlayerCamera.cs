d807298dcd68b6e2a1b3941a588f45c71aae10cc
Thorvik <deldemonho@gmail.com>
2018-10-30 13:42:57 +0100

    A 3th person kamera működik és bekötöttem egy karaktert

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -3,75 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour {
-	// Use this for initialization
-	public Transform lookAt;
-	public float MaxDistance = 5.0f;
-	public Transform target;
-	public float sensivityX = 10.0f;
-	public float sensivityY = 10.0f;
-	public float Y_ANGLE_MIN = -50.0f;
-	public float Y_ANGLE_MAX = 50.0f;
 
-	private float currentX = 0.0f;
+	public Transform lookAt;                    //Lookat of the camera
+    public Transform target;                    //Target, wich around it rotates
+    public float targetSizeViewportX = 1.0f;    //Size of the target in view space on x axis    
+    public float targetSizeViewportY = 1.0f;    //Same on y axis    
+    public float maxDistance = 5.0f;            //Maximum distance from the target
+
+    public float sensivityX = 10.0f;            //Camera move speed on y
+	public float sensivityY = 10.0f;            //Camera move speed on x
+    public float YAngleMin = -50.0f;            //Min degree on Y    
+	public float YAngleMax = 50.0f;             //Max degree on Y    
+
+    private float currentX = 0.0f;
 	private float currentY = 0.0f;
-	private Transform camTransform;
 	private float distance;
-	private Camera cam;
+    private float rayDistance;
 	private Vector3[] corners;
+    private Vector3[] offsets;
+
 	void Start () {
-		camTransform = transform;
-		cam = Camera.main;
-		corners = new Vector3[4];
-	}
+        corners = new Vector3[4];
+        offsets = new Vector3[4];
+        //Offset of the corners in target space
+        offsets[0].Set(targetSizeViewportX, targetSizeViewportY, 0);
+        offsets[1].Set(-targetSizeViewportX, targetSizeViewportY, 0);
+        offsets[2].Set(targetSizeViewportX, -targetSizeViewportY, 0);
+        offsets[3].Set(-targetSizeViewportX, -targetSizeViewportY, 0);
+        distance = maxDistance;
+        SetTransformation();
+        //Sets the raycast distance
+        rayDistance = (transform.rotation * offsets[0] + lookAt.position - (lookAt.position + maxDistance * transform.forward)).magnitude;  
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		currentX += Input.GetAxis ("Mouse X") * sensivityX;
-		currentY += Input.GetAxis ("Mouse Y") * sensivityY;
-
-		currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
-
-		countViewCorners ();
-		CheckCollision();
-
-		// TODO ezt sokszor kell hivni
-
-			
+	void Update () {        
+        currentX += Input.GetAxis ("Right Stick X") * sensivityX;
+		currentY += Input.GetAxis ("Right Stick Y") * sensivityY;
+		currentY = Mathf.Clamp (currentY, YAngleMin, YAngleMax);
+        distance = (transform.position - lookAt.position).magnitude;
+        CalculateViewCorners ();
+		CheckCollision();			
 	}
 
 	void LateUpdate(){
-		Vector3 dir = new Vector3 (0, 0, -distance);
-		Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
-
-		camTransform.position = target.position + rotation * dir;
-		camTransform.LookAt(lookAt.position);
+        SetTransformation();
 	}
 
+    //Sets up the new transformation of the camera
+    void SetTransformation(){
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
+        transform.position = target.position + rotation * dir;
+        transform.LookAt(lookAt.position);
+    }
 
-	//TODO ki kellene talalni, hogyan van az utkozes
+    //Calculates the new distance if there was an object between the lookat and the camera
 	private void CheckCollision(){
 		RaycastHit hitInfo;
-		foreach (var point in corners) {
-			if (Physics.Raycast(camTransform.position, point, out hitInfo, MaxDistance)) {
-				if(hitInfo.collider.tag == "enviroment")
-					distance = hitInfo.distance * 0.8f; 
-			}
+        Vector3 hitPoint = new Vector3();
+        float minDistance = rayDistance;
+
+        //Searches the nearest collision distance to the lookat
+        foreach (var point in corners) {
+			if (Physics.Raycast(point, transform.position - point, out hitInfo, rayDistance*1.2f)) {
+				    if(! (hitInfo.collider.tag == "Player"))
+                        if(hitInfo.distance < minDistance) {                           
+                            minDistance = hitInfo.distance;
+                            hitPoint = hitInfo.point;
+                    }                                    					    
+			}    
 		}
+
+        //Calculates the distance transformated for the camera forward axis
+        if (minDistance <= rayDistance){
+            Vector3 rayDir = (hitPoint - transform.position).normalized;
+            float cosalpha = Vector3.Dot(rayDir, transform.forward);
+            distance = Mathf.Abs(minDistance * cosalpha *  0.8f);
+        }           
+        else distance = maxDistance;
 	}
 
-	//TODO nem biztos hogy jo
-	private void countViewCorners(){
-		Vector3 RayDir = (lookAt.position - camTransform.position).normalized;
-
-		float offsetZ = cam.nearClipPlane;
-		float offsetX = offsetZ * Mathf.Tan (cam.fieldOfView / 2);
-		float offsetY = offsetX / cam.aspect;
-
-		Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
-
-		/*corners[0] = Vector3(-offsetX, offsetY, offsetZ) + camTransform.position + rotation * RayDir;
-		corners[1] = Vector3(offsetX, offsetY, offsetZ) + camTransform.position + rotation * RayDir;
-		corners[2] = Vector3(-offsetX, -offsetY, offsetZ) + camTransform.position + rotation * RayDir;
-		corners[3] = Vector3(offsetX, -offsetY, offsetZ) + camTransform.position + rotation * RayDir;*/
-	}
+    //Calculates the corners
+	private void CalculateViewCorners(){
+        for (int i = 0; i < 4; i++) {      
+            corners[i] = transform.rotation*offsets[i] + lookAt.position;
+        }
+    }
 }
